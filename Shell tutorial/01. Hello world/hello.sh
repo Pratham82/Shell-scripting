23cda0036060274460d0f9fd3037b76cfa80f33a
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-07 03:06:06 +0530

    Shell tutorial added

---
+++ Shell tutorial/01. Hello world/hello.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo 'Hello world'
+
+# We need modification access for running this command
+# For that we have to do the following
+# chmod +x hello.sh
+
---
+++ Shell tutorial/02. Variables and comments/Vars and comments.sh
@@ -0,0 +1,31 @@
+
+# This is a comment
+echo 'This a single line' # This is also an comment
+
+# Variables
+# Types: 
+# 1. System variables: Defined by OS, defined in Upper cases
+# 2. User defined variables: Created by users
+
+# System variables:
+echo $BASH # This line will return the name of the shell
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# Output:
+# This a single line
+# /usr/bin/bash
+# 5.0.17(1)-release
+# /home/pratham82
+# /home/pratham82/Dev/Shell-scripting/Shell tutorial/02. Variables and comments
+
+# User defined variables
+name=Prathamesh
+val1=10
+
+echo My name is $name
+echo Current shell dir $BASH
+echo Current shell version $BASH_VESION
+echo Current home directory $PWD
+echo Current val of val1:  $val1
